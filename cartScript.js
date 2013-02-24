38d91fd4eac58a8d11c839258800a29fdc1b6ea2
Brandon Leventhal <bleventh@gmail.com>
2013-02-24 15:43:42 -0800

    Reformatted code
    -JSONP methods did not need to be in the window event
    -Code makes more sense now and its much more clear
    -Allows calls to add events on dynamic element creation
    -Allows jsonp request to be made on an event call

--- cartScript.js
+++ cartScript.js
@@ -6,14 +6,100 @@ window.addEvent('domready', function(){
 
 
   //call back function
-  this.imageCallback = function(data)
+ 
+
+
+
+  
+
+  
+  //makes initial JSONP request to the apis
+  this.myJSONP();
+  
+
+  //Dynamically create elements for prototyping
+  for(var i = 0; i < 10; i++)
   {
-    console.log(data);
+      var anItem = new Element('div');
+      $(anItem).set('class', 'item');
+       //anItem = $(anItem).clone().cloneEvents(methods);
+      $(anItem).inject("items",'bottom');
+
+  };
+
+ 
+  this.addDrag($$('.item'));
+
+});
+
+
+var myJSONP = function(){ new Request.JSONP({
+    url: 'http://www.ifixit.com/api/1.0/topics',
+    callbackKey: 'jsoncallback',
+    log: true,
+    data: {
+        offset: offsetCount,
+        limit: '200',
+        jsonp: 'jsoncallback'
+    },
+    onRequest: function(url){
+        // a script tag is created with a src attribute equal to url
+
+    },
+    onComplete: function(data){
+        // the request was completed.
+
+    }
+
+}).send();
+};
+
+this.jsoncallback = function(data){
+  for(var i = 0; i < data.length; i++){
+    var apiID = data[i].topic.replace(/\s/g,"+");
+
+    var apiGet = "http://www.ifixit.com/api/1.0/topic/" + apiID;
+    var myJSONP = new Request.JSONP({
+            url: apiGet,
+            callbackKey: 'imageCallback',
+            log: true,
+            data: {
+                jsonp: 'imageCallback'
+            },
+            onRequest: function(url){
+                // a script tag is created with a src attribute equal to url
+                
+
+            },
+            onComplete: function(data){
+                // the request was completed.
+            },
+            onCancel: function()
+            {
+              console.log("error");
+            },
+            onTimeout: function()
+            {
+              console.log("error");
+            }
+
+          }).send();
+          //Increments offset
+          offsetCount += 200;
+        };
+      };
+
+ this.imageCallback = function(data)
+  {
+      if(data[0] == "error")
+      {
+        console.log("error");
+      }
       if(data.image.text!= null)
       {
         //the devices image url
         var aDeviceImage = data.image.text + '.thumbnail';
-        console.log(aDeviceImage);
+
 
 
         //adding a new device to the grid
@@ -37,74 +123,7 @@ window.addEvent('domready', function(){
         
   }
 
-  this.jsoncallback = function(data){
-        console.log(data);
-        for(var i = 0; i < data.length; i++){
-          var apiID = data[i].topic.replace(/\s/g,"+");
-          console.log(apiID);
-          var apiGet = "http://www.ifixit.com/api/1.0/topic/" + apiID;
-          console.log(apiGet);
-          var myJSONP = new Request.JSONP({
-            url: apiGet,
-            callbackKey: 'imageCallback',
-            log: true,
-            data: {
-                jsonp: 'imageCallback'
-            },
-            onRequest: function(url){
-                // a script tag is created with a src attribute equal to url
-                
 
-            },
-            onComplete: function(data){
-                // the request was completed.
-                console.log(data);
-            }
-
-          }).send();
-        };
-      };
-
-  /*Trying the json loads here*/
-  var theURL;
-
-  
-  //makes a JSONP request to the apis
-  var myJSONP = new Request.JSONP({
-    url: 'http://www.ifixit.com/api/1.0/topics',
-    callbackKey: 'jsoncallback',
-    log: true,
-    data: {
-        limit: '20',
-        jsonp: 'jsoncallback'
-    },
-    onRequest: function(url){
-        // a script tag is created with a src attribute equal to url
-
-    },
-    onComplete: function(data){
-        // the request was completed.
-        console.log(data);
-    }
-
-}).send();
-
-  
-
-  //Dynamically create elements for prototyping
-  for(var i = 0; i < 10; i++)
-  {
-      var anItem = new Element('div');
-      $(anItem).set('class', 'item');
-       //anItem = $(anItem).clone().cloneEvents(methods);
-      $(anItem).inject("items",'bottom');
-
-  };
-
- 
-  this.addDrag($$('.item'));
-
-});
 /*add the cart and drag methods to argument*/
 var addDrag = function(theListener) {
   theListener.addEvent('mousedown', function(event){
@@ -123,7 +142,6 @@ var addDrag = function(theListener) {
         onDrop: function(dragging, cart){
           dragging.destroy();
           if(cart != null){
-            console.log(cart);
             device.clone().inject(cart);
           cart.highlight('#7389AE', '#FFF');
 
